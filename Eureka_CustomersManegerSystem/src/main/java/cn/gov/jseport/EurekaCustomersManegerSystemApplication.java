dc4bb2bbdf920bda847a5e15a98128de0d76c888
puseon <puseon@163.com>
2019-04-25 15:41:57 +0800

    更改包名统一为：cn.gov.jseport

---
+++ Eureka_CustomersManegerSystem/src/main/java/cn/gov/jseport/EurekaCustomersManegerSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.gov.jseport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCustomersManegerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaCustomersManegerSystemApplication.class, args);
+	}
+
+}
